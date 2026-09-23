d2fa2a689505623ea80c8e17985aedde38a3456e
reyanshrai <reyanshrai05@gmail.com>
2026-09-23 19:47:11 +0530

    Add server.js to initialize the application and connect to the database

---
+++ Backend/url/Backend/src/server.js
@@ -0,0 +1,10 @@
+import app from "./app/app.js"
+import connectDB from "./config/db.js"
+await connectDB()
+
+const port = 3000
+
+app.listen(port,()=>{
+    console.log(`server is gonna on ${port} port`);
+    
+})
