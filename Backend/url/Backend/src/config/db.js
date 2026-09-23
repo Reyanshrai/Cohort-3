b8b38ee2a1bbdd6be4aeb107665a394eab988da6
reyanshrai <reyanshrai05@gmail.com>
2026-09-23 19:47:56 +0530

    Add db.js to establish MongoDB connection using Mongoose

---
+++ Backend/url/Backend/src/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose"
+import config from "./config.js"
+
+const connectDB = async ()=>{
+    await mongoose.connect(config.MONGODB_URI)
+    console.log("Db is connected")
+}
+
+export default connectDB
