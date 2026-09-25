048291db9a0a8fd9e4c790241905b17f45543558
reyanshrai <reyanshrai05@gmail.com>
2026-09-25 14:55:19 +0530

    Add auth.controller.js for user registration functionality

---
+++ Backend/Snitch/Server/src/controller/auth.controller.js
@@ -0,0 +1,48 @@
+import userModel from "../models/user.model.js";
+import bcrypt from "bcryptjs"
+import {createAccessToken,createRefreshToken} from "../utils/auth.utils.js"
+
+/**
+ * @description Register an user and save the data from req.body
+ * @param req express.Request
+ * @param req.body Object
+ * @param req.body.email String
+ * @param req.body.name String
+ * @param req.body.password String
+ */
+
+export const register = async (req,res) => {
+    const {email,name,password} = req.body
+
+    const isUserAlreadyExists = await userModel.findOne({
+        email
+    })
+
+    if(isUserAlreadyExists){
+        return res.status(400).json({
+            message : "User already exist with this email address ",
+            errors : [
+                {
+                    field : "email",
+                    message : 'User already exists with this email'
+                }
+            ]
+        })
+    }
+
+    const user = await userModel.create({
+        email,
+        name,
+        passwordHash : await bcrypt.hash(password , 12)
+    })
+
+    const accessToken = createAccessToken({
+        userId : user._id,
+        role : user.role
+    })
+
+    const refreshTokenToken = createRefreshToken({
+        userId : user._id,
+        role : user.role
+    })
+}
