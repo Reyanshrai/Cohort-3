525907f842131a453ff6669d07e1e7053fe035d5
reyanshrai <reyanshrai05@gmail.com>
2026-09-25 14:55:42 +0530

    Add auth.validator.js for user registration input validation

---
+++ Backend/Snitch/Server/src/validators/auth.validator.js
@@ -0,0 +1,34 @@
+import {body,validationResult} from "express-validator"
+
+export const registerValidator = [
+    body('email')
+        .exists().withMessage('Email is required').bail()
+        .trim()
+        .isEmail().withMessage('Enter valid email address'),
+
+    body("name")
+        .exists().withMessage('Name is required').bail()
+        .isString().withMessage('Name must be a String')
+        .trim()
+        .isLength({min : 2 , max : 50}).withMessage("Name length must be 2 to 50 character"),
+
+    body('password')
+        .exists().withMessage('Password is required').bail()
+        .isString().withMessage('Password must be a String')
+        .trim()
+        .isLength({min : 6}).withMessage("Password at least 6 character long"),  
+    (req,res,next) => {
+
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                message : "Invalid requests",
+                errors : errors.array()
+            })
+        }
+
+        next()
+    }      
+]
+
